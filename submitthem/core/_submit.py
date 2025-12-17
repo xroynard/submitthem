1e7b771a49d74c5e3f7bf4a926ac0eb6c3ffabdd
Xavier Roynard <xavier.roynard@safrangroup.com>
2025-12-17 17:39:48 +0100

    improve coverage

--- submitthem/core/_submit.py
+++ submitthem/core/_submit.py
@@ -6,6 +6,6 @@
 
 from submitthem.core.submission import submitthem_main
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma: no cover
     # This script is called by Executor.submit
     submitthem_main()
